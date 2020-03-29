8fbef95d4d52c486846019d6119336c1e7767079
Pavel Horal <pavel.horal@orchitech.cz>
2020-03-29 01:33:05 +0100

    Add code coverage reporting.

--- test/utils/renderYamlLike.spec.ts
+++ test/utils/renderYamlLike.spec.ts
@@ -5,38 +5,46 @@ import renderYamlLike from "../../src/utils/renderYamlLike";
 describe("renderYamlLike", () => {
 
   it("renders simple object", () => {
-    expect(renderYamlLike({ FOO: 1, BAR: 2 })).toEqual(
+    expect(renderYamlLike({ FOO: 1, BAR: 2 })).toBe(
         "FOO: 1\n" +
         "BAR: 2");
   });
 
   it("renders simple array", () => {
-    expect(renderYamlLike([ "FOO", "BAR" ])).toEqual(
+    expect(renderYamlLike([ "FOO", "BAR" ])).toBe(
         "- FOO\n" +
         "- BAR");
   });
 
   it("renders array with object", () => {
-    expect(renderYamlLike([{ "FOO": 1, "BAR": 2 }])).toEqual(
+    expect(renderYamlLike([{ "FOO": 1, "BAR": 2 }])).toBe(
         "- FOO: 1\n" +
         "  BAR: 2");
   });
 
   it("renders object with array", () => {
-    expect(renderYamlLike({ "FOO": [ "BAR", "BAZ" ] })).toEqual(
+    expect(renderYamlLike({ "FOO": [ "BAR", "BAZ" ] })).toBe(
         "FOO:\n" +
         "- BAR\n" +
         "- BAZ");
   });
 
+  it("renders object with empty array", () => {
+    expect(renderYamlLike({ "FOO": [] })).toBe("FOO:");
+  });
+
+  it("renders buffer as hex string", () => {
+    expect(renderYamlLike(Buffer.from("01020304", "hex"))).toBe("[01020304]");
+  });
+
   it("renders object with object", () => {
-    expect(renderYamlLike({ "FOO": { "BAR": "BAZ" } })).toEqual(
+    expect(renderYamlLike({ "FOO": { "BAR": "BAZ" } })).toBe(
         "FOO:\n" +
         "  BAR: BAZ");
   });
 
   it("renders array with array", () => {
-    expect(renderYamlLike([["FOO"], ["BAR", "BAZ"]])).toEqual(
+    expect(renderYamlLike([["FOO"], ["BAR", "BAZ"]])).toBe(
         "-\n" +
         "  - FOO\n" +
         "-\n" +
